65ea39ba0131ee5b57545863588bdb9ccad2935e
Shawna Carey <scareycs@gmail.com>
2019-04-26 15:40:41 -0400

    Added mailchimp subscribe form

--- index.js
+++ index.js
@@ -32,3 +32,17 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+
+function($) {
+  window.fnames = new Array(); window.ftypes = new Array();
+  fnames[0]='EMAIL';
+  ftypes[0]='email';
+  fnames[1]='FNAME';
+  ftypes[1]='text';
+  fnames[2]='LNAME';
+  ftypes[2]='text';}
+
+(jQuery);
+
+var $mcj = jQuery.noConflict(true);
