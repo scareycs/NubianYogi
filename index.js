3938f65caf1459c140d0319548f6ac717225c7e5
Shawna Carey <scareycs@gmail.com>
2019-05-19 09:35:58 -0400

    Deletion
    Deleted read more or less function. and subscription functionality for restructuring

--- index.js
+++ index.js
@@ -16,33 +16,3 @@ $('#menu').click(function(){
   $(this).toggleClass('open');
   $('.navContent').slideToggle('fast','linear');
 });
-
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more"; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less"; 
-    moreText.style.display = "inline";
-  }
-}
-
-
-function() {
-  window.fnames = new Array(); window.ftypes = new Array();
-  fnames[0]='EMAIL';
-  ftypes[0]='email';
-  fnames[1]='FNAME';
-  ftypes[1]='text';
-  fnames[2]='LNAME';
-  ftypes[2]='text';}
-
-(jQuery);
-
-var $mcj = jQuery.noConflict(true);
